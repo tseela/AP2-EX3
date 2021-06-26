3d33587e32cc949b8fcf515e4b4aa1caea03c3fd
Tal Ben-zvi <talbz03@gmail.com>
2021-06-26 20:28:26 +0300

    added data binding

--- app/src/main/java/tseela/school/ap2_ex3/FlightViewModel.java
+++ app/src/main/java/tseela/school/ap2_ex3/FlightViewModel.java
@@ -4,8 +4,7 @@ public class FlightViewModel implements IFlightViewModel {
     private static final int SEEK_BAR_TICK_SCALAR = 100;
 
     private IFlightModel model;
-    public String ip;
-    public int port;
+    public String ip, port;
     public int rudder, throttle;
 
     public FlightViewModel() {
@@ -14,7 +13,14 @@ public class FlightViewModel implements IFlightViewModel {
 
     @Override
     public boolean connect() {
-        return model.connect(ip, port);
+        int portNum;
+        try {
+            portNum = Integer.parseInt(port);
+            return model.connect(ip, portNum);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
